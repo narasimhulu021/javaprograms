0ed3d24acb74802c28ba797a382a509813d68b48
G V Narasimhulu <venkatanarasimhulu021@gmail.com>
2023-05-30 10:50:15 +0530

    number

---
+++ prime.java
@@ -0,0 +1,15 @@
+/**
+ * prime
+ */
+public class prime {
+
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.print(" * ");
+                System.out.print(" hi ra mawa ");
+            }
+            System.out.println();
+        }
+    }
+}
